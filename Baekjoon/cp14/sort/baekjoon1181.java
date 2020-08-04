d2bbe19fdc3da37efe52afb664091527bd9202a1
Ever <hsik0225@gmail.com>
2020-08-04 14:25:30 +0900

    [정렬] 2750 클래스 이름 수정

---
+++ Baekjoon/cp14/sort/baekjoon1181.java
@@ -0,0 +1,20 @@
+package cp14.sort;
+
+/*
+
+https://www.acmicpc.net/problem/2750
+
+*/
+
+import java.util.Scanner;
+
+public class baekjoon1181 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        while (n-- > 0) {
+
+        }
+    }
+}
