cbed1342c819ee251bcfe9ae66e7bff3980f6bc3
Ever <hsik0225@gmail.com>
2020-08-06 16:23:56 +0900

    [정렬] 11650 풀이

--- Baekjoon/cp14/sort/Baekjoon11650.java
+++ Baekjoon/cp14/sort/Baekjoon11650.java
@@ -4,17 +4,28 @@ package cp14.sort;
 
 https://www.acmicpc.net/problem/11650
 
+2차원 평면 위의 점 N개가 주어진다.
+
+좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+
+합병 정렬로 풀이
 */
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Baekjoon11650 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while (n-- > 0) {
+        int n = Integer.parseInt(br.readLine());
 
-        }
+        String[] strArray = new String[n];
+        while (n-- > 0)
+            strArray[n] = br.readLine();
+
+        int[] xArray = new int[strArray.length];
+        int[] yArray = new int[strArray.length];
     }
 }
