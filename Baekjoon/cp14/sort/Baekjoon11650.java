e0810ea8e5ea2d25e7ee02c1d56a2b0cbafae6e7
Ever <hsik0225@gmail.com>
2020-08-06 21:26:38 +0900

    [정렬] 11650 풀이

--- Baekjoon/cp14/sort/Baekjoon11650.java
+++ Baekjoon/cp14/sort/Baekjoon11650.java
@@ -16,16 +16,91 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Baekjoon11650 {
+    public static String[] numberStrings;
+    public static String[] tempStrings;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
-        String[] strArray = new String[n];
+        numberStrings = new String[n];
+        tempStrings = new String[n];
         while (n-- > 0)
-            strArray[n] = br.readLine();
+            numberStrings[n] = br.readLine();
 
-        int[] xArray = new int[strArray.length];
-        int[] yArray = new int[strArray.length];
+        mergeSort(0, numberStrings.length - 1);
+
+        for (String s : tempStrings) {
+            System.out.println(s);
+        }
+    }
+
+    private static final void mergeSort(int low, int high) {
+        if (high <= low)
+            return;
+
+        int mid = low + (high - low) / 2;
+
+        // 앞부분 재귀호출
+        mergeSort(low, mid);
+
+        // 뒷부분 재귀호출
+        mergeSort(mid + 1, high);
+
+        // 합병 수행
+        merge(low, mid, high);
+    }
+
+    private static void merge(int low, int mid, int high) {
+        int i = low;
+        int j = mid + 1;
+
+        String[] iSplit;
+        String[] jSplit;
+
+        for (int k = low; k <= high; k++) {
+
+            // 앞부분이 먼저 소진된 경우
+            if (i > mid) {
+                tempStrings[k] = numberStrings[j++];
+                continue;
+            }
+
+            // 뒷부분이 먼저 소진된 경우
+            else if (j > high) {
+                tempStrings[k] = numberStrings[i++];
+                continue;
+            }
+
+            iSplit = numberStrings[i].split(" ");
+            int iX = Integer.parseInt(iSplit[0]);;
+            int iY = Integer.parseInt(iSplit[1]);
+
+            jSplit = numberStrings[j].split(" ");
+            int jX = Integer.parseInt(jSplit[0]);
+            int jY = Integer.parseInt(jSplit[1]);
+
+            // a[j] 가 승자
+            if (jX < iX)
+                tempStrings[k] = numberStrings[j++];
+
+            else if (jX == iX) {
+                if (jY < iY)
+                    tempStrings[k] = numberStrings[j++];
+
+                else
+                    tempStrings[k] = numberStrings[i++];
+            }
+
+            // a[i] 가 승자
+            else
+                tempStrings[k] = numberStrings[i++];
+        }
+
+        // 보조배열 b를 배열 a에 복사
+        for (int k = low; k <= high; k++) {
+            numberStrings[k] = tempStrings[k];
+        }
     }
 }
