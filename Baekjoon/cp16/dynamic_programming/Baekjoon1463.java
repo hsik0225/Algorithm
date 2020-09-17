8d8c3163e0b4fbc98351417fa127535ba4a2c49f
naru <hsik0225@gmail.com>
2020-09-17 13:00:59 +0900

    [동적계획법1] 1463, 1932, 2579 풀이

---
+++ Baekjoon/cp16/dynamic_programming/Baekjoon1463.java
@@ -0,0 +1,45 @@
+package cp16.dynamic_programming;
+
+/*
+
+https://www.acmicpc.net/problem/1463
+
+*/
+
+import java.util.Scanner;
+
+public class Baekjoon1463 {
+    private static int[] cases;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        cases = new int[n + 1];
+        System.out.println(dp(n));
+    }
+
+    public static int dp(int input) {
+        if (input == 1) {
+            return 0;
+        }
+
+        if (cases[input] > 0) {
+            return cases[input];
+        }
+
+        cases[input] = dp(input - 1) + 1;
+
+        int temp;
+        if (input%2 == 0) {
+            temp = dp(input/2) + 1;
+            cases[input] = Math.min(cases[input], temp);
+        }
+
+        if (input%3 == 0) {
+            temp = dp(input/3) + 1;
+            cases[input] = Math.min(cases[input], temp);
+        }
+
+        return cases[input];
+    }
+}
---
+++ Baekjoon/cp16/dynamic_programming/Baekjoon1932.java
@@ -0,0 +1,39 @@
+package cp16.dynamic_programming;
+
+/*
+
+https://www.acmicpc.net/problem/1932
+
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon1932 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] triangle = new int[n + 1][n + 1];
+
+        StringTokenizer st;
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= i; j++) {
+                triangle[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                triangle[i][j] += Math.max(triangle[i - 1][j - 1], triangle[i - 1][j]);
+                max = Math.max(triangle[i][j], max);
+            }
+        }
+
+        System.out.println(max);
+    }
+}
