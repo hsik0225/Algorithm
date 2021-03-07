7947bee9a01d441737d1eebf49bba81843e47132
Ever <hsik0225@gmail.com>
2021-03-07 23:27:54 +0900

    백준 1934

---
+++ Baekjoon/cp16/math3/Baekjoon1934.java
@@ -0,0 +1,35 @@
+package cp16.math3;
+
+import java.util.Scanner;
+
+public class Baekjoon1934 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            long a = scanner.nextLong();
+            long b = scanner.nextLong();
+
+            if (a > b) {
+                long temp = a;
+                a = b;
+                b = temp;
+            }
+
+            long gcd = gcd(a, b);
+
+            System.out.println(a * b / gcd);
+        }
+    }
+
+    private static long gcd(long a, long b) {
+        while (b != 0) {
+            long r = a % b;
+            a = b;
+            b = r;
+        }
+
+        return a;
+    }
+}
